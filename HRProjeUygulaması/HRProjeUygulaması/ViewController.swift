3d99d49f747233568656e28fa8a8cdef7be8d6ed
Burak Dünya <burakdunya@icloud.com>
2024-12-31 00:49:18 +0300

    login update
    login sayfası düzenlendi

---
+++ HRProjeUygulaması/HRProjeUygulaması/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HRProjeUygulaması
+//
+//  Created by Burak DÜNYA on 28.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HRProjeUygulaması/HRProjeUygulamasıTests/HRProjeUygulamas_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HRProjeUygulamas_Tests.swift
+//  HRProjeUygulamasıTests
+//
+//  Created by Burak DÜNYA on 28.12.2024.
+//
+
+import Testing
+@testable import HRProjeUygulamas_
+
+struct HRProjeUygulamas_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
